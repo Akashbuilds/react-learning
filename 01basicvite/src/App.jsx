aec7a21859e05acdc401dc9f11d8675afca23048
Akash Goswami <akashgoswami70870@gmail.com>
2024-02-14 11:48:15 +0530

    Understanding react functioning

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -3,11 +3,11 @@ import Youtube from "./Youtube"
 
 function App() {
 
-
+const user = "Akash";
   return (
     <>
   <h1> Vite React App</h1>
-  <h1>Vite react app again</h1>
+  <h1>{user} is using Vite to fire React</h1>
   <Youtube/>  
       
   </>)
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -2,9 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement = {
+    type: "a",
+  
+    features: {
+      href: "https://chat.openai.com/",
+      target: "_blank",
+    },
+  
+    children: "Click me to visit openai",
+  };
 
+  function MyApp(){
+    return (
+        <div>
+            custom react app
+        </div>
+    )
+  }
+
+  const anotherElement = (
+    <a href = "https://chat.openai.com/" target = "_blank">Visit OpenAi</a>
+  )
+
+  const aReactElelement = React.createElement(
+    'a',
+    {href: "https://chat.openai.com/", target: "_blank"},
+    'Visit OpenAi',
+  )
+
+  
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <App />
+  aReactElelement
 
 )
